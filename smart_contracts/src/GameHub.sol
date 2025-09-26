83ef46648d89db4b730e5c23a74de981feb482a2
blockvished <jcmvishael@gmail.com>
2025-09-26 19:57:22 +0000

    refactor code and add activate feature

--- smart_contracts/src/GameHub.sol
+++ smart_contracts/src/GameHub.sol
@@ -5,38 +5,76 @@ import { GameRegistry } from "./GameFactory.sol";
 
 contract GameHub is GameRegistry {
 
-    struct Event {
+    uint256 public gameEventCount = 1;
+
+    struct GameEvent {
+        bool active;
+        string eventName;
         uint256 startTime;
         uint256 endTime;
         uint256 referencedGameId;
         uint256 durationMinutes;
         uint256 minStakeAmt;
         uint256 pooledAmt;
+        bool scoresFinalized;
+        uint256 playersCount;
+        uint256 winnersCount;
     }
-
-    uint256 public gameEventCount = 1;
 
 
     mapping(uint256 => mapping(address => bool)) public joined;
-    mapping(uint256 => Event) public gamesEvents;
+    mapping(uint256 => GameEvent) public gamesEvents;
 
 
-    function createGameEvent(uint256 id, uint256 startTime, uint256 durationMinutes, uint256 minStakeAmt) external {
+    function createGameEvent(string calldata eventName, uint256 durationMinutes, uint256 minStakeAmt, uint256 winnersCount, bool activate) external {
         require(durationMinutes > 10, "duration must be > 10 minutes");
 
-        Event storage ge = gamesEvents[id];
+        uint256 gameEventId = gameEventCount;
+        GameEvent storage ge = gamesEvents[gameEventCount];
 
-        ge.startTime = block.timestamp;
-        ge.endTime = startTime + (durationMinutes * 1 minutes);
+        uint256 startTime;
+        uint256 endTime;
 
+        if (activate) {
+            startTime = block.timestamp;
+            endTime = startTime + (durationMinutes * 1 minutes);
+            ge.startTime = startTime;
+            ge.endTime = endTime;
+        } else {
+            startTime = 0;
+            endTime = 0;
+        }
+
+        ge.eventName = eventName;
+        ge.referencedGameId = gameEventId;
+
+        ge.active = activate;
         ge.durationMinutes = durationMinutes;
         ge.minStakeAmt = minStakeAmt;
+        ge.winnersCount = winnersCount;
+        ge.scoresFinalized = false;
+
+        gameEventCount += 1;
     }
 
-    function joinGame(uint256 id) external payable {
-        Event storage ge = gamesEvents[id];
+
+    function joinGame(uint256 gameEventId) external payable {
+        GameEvent storage ge = gamesEvents[gameEventId];
+
         ge.pooledAmt += msg.value;
 
-        joined[id][msg.sender] = true;
+        joined[gameEventId][msg.sender] = true;
+        ge.playersCount += 1;
     }
+
+    function activateGame(uint256 gameEventId) external {
+        GameEvent storage ge = gamesEvents[gameEventId];
+        uint256 startTime = block.timestamp;
+        uint256 endTime = startTime + (ge.durationMinutes * 1 minutes);
+        ge.startTime = startTime;
+        ge.endTime = endTime;
+        ge.active = true;
+    }
+
+    receive() external payable {}
 }
