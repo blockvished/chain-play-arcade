f2ea82e7538bfb17b362619eff5c82701a3fd0de
blockvished <jcmvishael@gmail.com>
2025-09-26 17:13:26 +0000

    create game state with id

---
+++ smart_contracts/src/GameFactory.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract GameRegistry {
+    struct GameDefinition {
+        uint256 id;
+        string name;
+        string image;      
+        string description;
+    }
+
+    uint256 public gameIdCount = 1;
+
+    constructor() {}
+
+    mapping(uint256 gameId => GameDefinition) private gameDefinitions;
+
+    function createGameDefinition(
+        string calldata name,
+        string calldata image,
+        string calldata description
+    ) public {
+        uint256 gameId = gameIdCount;
+
+        GameDefinition memory newGame = GameDefinition({
+            id: gameId,
+            name: name,
+            image: image,
+            description: description
+        });
+
+        gameDefinitions[gameId] = newGame;
+
+        gameIdCount++;
+    }
+
+    function getGameDefinition(uint256 gameId) public view returns (GameDefinition memory) {
+        return gameDefinitions[gameId];
+    }
+
+}
