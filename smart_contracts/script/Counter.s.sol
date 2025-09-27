8cbe374d4bd6fde0728fe9085da84a8b9cd2c44c
PAVANTEJ-05 <nptpavantej79@gmail.com>
2025-09-27 09:46:09 +0530

    scripts for nft

--- smart_contracts/script/Counter.s.sol
+++ smart_contracts/script/Counter.s.sol
@@ -1,19 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {SVGNFT} from "../src/Nft.sol";
 
 contract CounterScript is Script {
-    Counter public counter;
+    SVGNFT public counter;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        counter = new SVGNFT();
 
+        counter.create(1);
+
+        counter.create(2);
+
+         counter.update(1,"<svg width='500' height='500' viewBox='0 0 285 350' fill='none' xmlns='http://www.w3.org/2000/svg'><path fill='black' d='M150,0,L75,200,L225,200,Z'></path></svg>");
+     
         vm.stopBroadcast();
     }
 }
